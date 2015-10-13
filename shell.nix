95c754bb043ce148f907dbd6f25039cac2af562e
Utku Demir <utdemir@gmail.com>
2015-10-13 06:44:39 +0000

    Use ghc7102 on shell.nix

--- shell.nix
+++ shell.nix
@@ -1,13 +1,13 @@
 with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
+let pkg = haskell.packages.ghc7102.callPackage
             ({ mkDerivation, base, bytestring, directory, doctest, filepath
-             , stdenv, text
+             , stdenv, text, cabal-install
              }:
              mkDerivation {
                pname = "cef";
                version = "0.1.2";
                src = ./.;
-               buildDepends = [ base bytestring text ];
+               buildDepends = [ base bytestring text cabal-install ];
                testDepends = [ base directory doctest filepath ];
                homepage = "http://github.com/picussecurity/haskell-cef.git";
                description = "CEF log format";

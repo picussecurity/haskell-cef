a175078fea8786eaf4767633974223d3a103bd53
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2018-03-13 15:24:40 +0300

    Updated default.nix

--- default.nix
+++ default.nix
@@ -3,10 +3,10 @@
 }:
 mkDerivation {
   pname = "cef";
-  version = "0.1.2";
+  version = "0.1.5";
   src = ./.;
-  buildDepends = [ base bytestring text time ];
-  testDepends = [ base directory doctest filepath ];
+  libraryHaskellDepends = [ base bytestring text time ];
+  testHaskellDepends = [ base directory doctest filepath ];
   homepage = "http://github.com/picussecurity/haskell-cef.git";
   description = "CEF log format";
   license = stdenv.lib.licenses.bsd3;
